08fcf5dd80039d7862afd545007e0d935a460dc5
Matthew Couch <mcouch@gmail.com>
2022-04-21 13:21:10 -0500

    Build API test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ngAPIDemo';
+  url = '/api/getRoles';  //Change to whatever you want the default URL to be
+  result = 'Results here';
+  waiting = false;
+
+  constructor(private http:HttpClient){}
+
+  public logout() {
+    this.url = '/logout';  //Change to logout URL
+    this.callServer();
+  }
+
+  public callServer() {
+    this.waiting = true;
+    this.http.get(this.url).subscribe({
+      next: (r) => {
+        this.result = JSON.stringify(r);
+        this.waiting = false;
+      },
+      error: (e) => {
+        this.result = JSON.stringify(e);
+        this.waiting = false;
+      }
+    });
+  }
 }
